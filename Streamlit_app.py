e583e10f9d56e75b523601c8fe96209b2461d37a
sushmashindehub <joinsushma@gmail.com>
2025-04-24 12:00:49 +0800

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -16,7 +16,7 @@ st.write(
 title = st.text_input("Name on your smoothie:")
 st.write("The name on your smoothie will be:", title)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col(SEARCH_ON))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width = True)
 st.stop()
 
