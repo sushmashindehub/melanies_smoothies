c1d4af5125b884b55044d59d4b7617d8c7708224
sushmashindehub <joinsushma@gmail.com>
2025-04-24 12:07:17 +0800

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
-import pandas pd
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 cnx = st.connection("snowflake")
