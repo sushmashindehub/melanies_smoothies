6c3f5e3ad44afb42298d4123142e109b9e5125b1
sushmashindehub <joinsushma@gmail.com>
2025-04-24 13:44:07 +0800

    Add files via upload

---
+++ Pending Smoothie Orders.py
@@ -0,0 +1,34 @@
+# Import python packages
+import streamlit as st
+
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+# Write directly to the app
+st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
+st.write(
+  """Orders that need to be filled:
+  """
+)
+
+session = get_active_session()
+#my_dataframe = session.table("smoothies.public.ORDERS").select(col('NAME_ON_ORDER'), col('order_filled'))
+my_dataframe = session.table("smoothies.public.ORDERS").filter(col('order_filled')==0)
+
+if my_dataframe.count() != 0:
+    editable_df = st.data_editor(my_dataframe)
+    submitted = st.button('Submit')
+    if submitted:
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+        try:
+            og_dataset.merge(edited_dataset
+                     , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                     , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                )
+            st.success('Someone clicked the button', icon = '👍')
+        except:
+            st.write("Something went wrong")
+else:
+    st.success('There are no pending orders now', icon = '👍')
+    
